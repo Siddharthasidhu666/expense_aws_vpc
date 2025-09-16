925b883d6bac416517213b4d96fcebf06023d469
siddhu <siddhumahesh333@gmail.com>
2025-09-16 15:36:28 +0530

    subnets output

--- outputs.tf
+++ outputs.tf
@@ -4,11 +4,21 @@ output "azs" {
 }
 
 output "vpc_id" {
-    value = aws_vpc.main.id
-  
+  value = aws_vpc.main.id
+
 }
 
 output "public_subnet_ids" {
-    value = aws_subnet.public-subnet[*].id
-    
+  value = aws_subnet.public-subnet[*].id
+
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private-subnet[*].id
+
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database-subnet[*].id
+
 }
