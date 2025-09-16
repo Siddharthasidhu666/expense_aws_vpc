93ace9dab1b18f2c0ca30d0ccd36bde2cf7275b2
siddhu <siddhumahesh333@gmail.com>
2025-09-16 15:29:49 +0530

    public subnets output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
 output "azs" {
-    value = data.aws_availability_zones.available
-  
+  value = data.aws_availability_zones.available
+
 }
 
 output "vpc_id" {
     value = aws_vpc.main.id
   
+}
+
+output "public_subnet_ids" {
+    value = aws_subnet.public-subnet[*].id
+    
 }
